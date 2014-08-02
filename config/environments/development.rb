bbc7d2f5c56292bee10a7e3c4ce63e28d363e3d4
Jodi Detch <jodidetch@gmail.com>
2014-08-01 23:42:25 -0400

    Configuration for development environment no longer allows caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,8 +11,8 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
-  # Turns on caching for development environment
+  config.action_controller.perform_caching = false
+  # Turns on/off caching for development environment
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
